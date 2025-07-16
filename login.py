a12558553a63762266667623c41d14688ee08bad
ai7287 <ai72877@naver.com>
2025-07-16 15:05:16 +0900

    fix: 로그인 기능 (house)

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 def login(id,pw):
     if id == "admin" and pw == "1234":
-        print(  "로그인 성공!")
+        print(  "로그인 성공!ㅋ")
     else:
-        print("로그인 실패! 아이디 또는 비밀번호가 잘못되었습니다.")
+        print("로그인 실패!ㅎ 아이디 또는 비밀번호가 잘못되었습니다.")
